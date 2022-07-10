9d8512e1cb40ba6fd9db03dcfe5a7c203ecc4f9f
Dandara-Aryadne <dandaraaryadne@hotmail.com>
2022-07-09 21:37:01 -0300

    atualizando o responsivo

--- src/App.js
+++ src/App.js
@@ -15,112 +15,107 @@ const GlobaStyle = createGlobalStyle`
   }
 `
 const Container = styled.main`
-position:relative;
-display: flex;
-justify-contnnt:center;
-align-items:center;
-width:100%;
-height:100vh;
-`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
 
 const Div = styled.div`
-position:absolute;
-display:flex;
-width:30%;
-align-items:center;
-height:30vh;
-justify-content:center;
-margin-bottom:30%;
-left:32vw;
+  position: absolute;
+  display: flex;
+  width: 30%;
+  align-items: center;
+  height: 30vh;
+  justify-content: center;
+  margin-bottom: 20%;
+  left: 32vw;
 
-@media(max-width:600px){
- width:50%;
- height:20vh;
- left:23vw;
- bottom:40%;
-}
-`
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+    width: 50%;
+    height: 20vh;
+    left: 27.3vw;
+    bottom: 70%;
+  }
+`;
 
 const Nav = styled.div`
-position:relative;
-display:flex;
-justify-content:center;
-align-items:center;
-width:10%;
-height:9vh;
-left:42.5vw;
-top:42vh;
-@media(max-width:600px){
-
-width:30%;
-height:11vh;
-left:32vw;
-top:42vh;
-
-}
-`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10%;
+  height: 9vh;
+  left: 20vw;
+  top: 42vh;
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+    width: 70%;
+    height: 10vh;
+    left: 34vw;
+    top: 29vh;
+  }
+`;
 const Button = styled.button`
-position:absolute;
-width:80%;
-height:70%;
-border-radius: 30px 30px 30px;
-background-color: rgb(88, 152, 200 );
-color:white;
-border:none;
-box-shadow:2px 4px 2px #95ECF3;
-@media(max-width:600px){
   position: absolute;
-
-  width:60%;
-  height:90%;
-}
-`
+  width: 80%;
+  height: 70%;
+  border-radius: 30px 30px 30px;
+  background-color: rgb(88, 152, 200);
+  color: white;
+  border: none;
+  box-shadow: 2px 4px 2px #95ecf3;
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+    position: absolute;
+    width: 80%;
+    height: 90%;
+  }
+`;
 
 const Section = styled.div`
-display:flex;
-position:relative;
-width:50%;
-height:55vh;
-left:12vw;
-top:11vh;
-@media (max-width:600px){
- 
-  display:flex;
-  margin-bottom:10vh;
-  margin-left:-19vw;
-  justify-content:center;
-}
-`
+  display: flex;
+  position: relative;
+  width: 50%;
+  height: 55vh;
+  right: 10vw;
+  top: 11vh;
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+    display: flex;
+    margin-bottom: 25vh;
+    margin-right: 40vw;
+    justify-content: center;
+  }
+`;
 
 const Img = styled.img`
-display:flex;
-position:absolute;
-top:2vh;
-left:5%;
-width: 90%;
-height: 50vh;
-object-fit: fill;
-@media (max-width:600px){
-  
-  box-shadow: 1px 0px 2px 5px #DAF7A6 ;
-  border-radius: 35px 35px 35px 35px;
-  width:38vw;
-  left:5vw;
-  
-}
-`
-const H1= styled.h1`
-color: #23ACCB;
-font-size: 10vw;
-font-family: 'Rubik Moonrocks', cursive;
-` 
+  display: flex;
+  position: absolute;
+  top: 2vh;
+  left: 5%;
+  width: 90%;
+  height: 50vh;
+  object-fit: fill;
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+    box-shadow: 1px 0px 2px 5px #daf7a6;
+    border-radius: 35px 35px 35px 35px;
+    height: 35vh;
+    width: 60vw;
+    left: -4vw;
+  }
+`;
+const H1 = styled.h1`
+  color: #23accb;
+  font-size: 10vw;
+  font-family: "Rubik Moonrocks", cursive;
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+  }
+`;
 
-
-
-export default function App(){
-
-const [doguinho,setDoguinho]= useState([])
-const [darkMode, setDarkMode]=useState(false)
+export default function App() {
+  const [doguinho, setDoguinho] = useState(false, []);
+  const [darkMode, setDarkMode] = useState(false);
+  const [modal, SetModal] = useState(false);
 
 function Card(){
    axios.get('https://dog.ceo/api/breeds/image/random').then((responseDog)=>{
@@ -149,7 +144,7 @@ const theme=createTheme({
     <Button onClick={()=>{Card()}}>FIND A DOG</Button>
     </Nav>
     <Section>
-    <Img src={doguinho} alt="What u will find?"/>
+    <Img src={doguinho}/>
     </Section>
     </Container>
     </Paper>
